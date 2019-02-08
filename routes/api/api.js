f6780e9520a1abc0bc0c22ec2429508dda50ca1a
Eduardo <eduardofc97@hotmail.com>
2019-02-07 18:30:23 -0600

    api

--- routes/api/api.js
+++ routes/api/api.js
@@ -6,8 +6,16 @@ var thingsApi = require('./api/things');
 
 router.route('/users', userApi);
 router.route('/things', thingsApi);
-
-
+var Prueba=[];
+var _Prueba = {
+    _id: uuidv4,
+    empresa: 'Google',
+    url: 'www.youtube.com',
+    nombre:'nombre',
+    year: '2019',
+    rating:'5',
+    fechaIng: Date
+};
 
 
 
